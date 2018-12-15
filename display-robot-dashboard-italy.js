77ee54a8ccb6df5336d75aacf979146e2a14fd42
jairo <jairo.araujo.silva@gmail.com>
2018-12-14 22:27:23 -0200

    scroll automatico do italia dashboard

--- display-robot-dashboard-italy.js
+++ display-robot-dashboard-italy.js
@@ -1,11 +1,29 @@
 'use strict';
 
-(function display_robot_dashboard_italy() {
-    console.log('running display_robot_dashboard_italy');
-    var el = document.getElementsByClassName('reveal-overlay')[1];
-    if (el) 
-        el.style.display = 'none';
-    var wait_time_before_go = 10000;
-    var next_url = "https://app.rupee.com.br/dashboard_mundo";
-    setTimeout(() => window.location.href = next_url, wait_time_before_go);
+(function display_robot_company_panel() {
+    console.log('running display_robot_company_italy');
+    var g = () => {
+        var last_window_pageyoffset = -1;
+        return () => {
+            try {
+                var reached_bottom = ((window.pageYOffset - last_window_pageyoffset) == 0);
+                if (!reached_bottom) {
+                    last_window_pageyoffset = window.pageYOffset;
+                    console.log(window.pageYOffset - last_window_pageyoffset);
+                    window.scrollBy(0, 8);
+                } else {
+                    clearInterval(drcp_interval);                    
+                    var next_url = "https://app.rupee.com.br/dashboard_mundo";
+                    console.log('vazando daqui', next_url);
+                    setTimeout(() => window.location.href = next_url, wait_time_before_go);
+                }
+            } catch (err) {
+                clearInterval(drcp_interval);
+                throw err;
+            }
+        }
+    };
+    var wait_time_before_go = 1000;
+    var scroll_speed = 200;
+    var drcp_interval = setInterval(g(), scroll_speed);
 })();
